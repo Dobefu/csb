90aeb63d4d6e852dc2f6201e9e3da1b9c37082c1
Connor van Spronssen <info@dobefu.com>
2024-11-22 14:04:52 +0100

    Add logic to read a .env file upon starting

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
+import "github.com/joho/godotenv"
+
 func init() {
-	// Init.
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		panic("No .env file found. Please copy it from the .env.example and enter your credentials.")
+	}
 }
 
 func main() {
