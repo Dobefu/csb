20538a5612d0aaf399ddee43240245a22f843bd9
Connor van Spronssen <info@dobefu.com>
2024-11-23 14:58:47 +0100

    Add a published column to the routes table

--- db/migrations/000001_create_routes_table.up.sql
+++ db/migrations/000001_create_routes_table.up.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS routes(
   uuid varchar(64) NOT NULL PRIMARY KEY UNIQUE,
   slug varchar(255) NOT NULL,
   url varchar(255) NOT NULL,
-  parent varchar(64)
+  parent varchar(64),
+  published boolean NOT NULL DEFAULT 0
 );
