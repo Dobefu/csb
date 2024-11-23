876efd8c5c7388d936b6a8a51be58469039dc3e2
Connor van Spronssen <info@dobefu.com>
2024-11-23 16:26:14 +0100

    Rename the uuid column to uid

--- db/migrations/000001_create_routes_table.up.sql
+++ db/migrations/000001_create_routes_table.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS routes(
-  uuid varchar(64) NOT NULL PRIMARY KEY UNIQUE,
+  uid varchar(64) NOT NULL PRIMARY KEY UNIQUE,
   contentType varchar(255) NOT NULL,
   slug varchar(255) NOT NULL,
   url varchar(255) NOT NULL,
