c95bedff0cf3b18db6344b5b67a70e0f41afa261
Connor van Spronssen <info@dobefu.com>
2024-11-23 16:02:39 +0100

    Implement logic for pagination

--- cmd/cs_sdk/sync.go
+++ cmd/cs_sdk/sync.go
@@ -7,34 +7,53 @@ import (
 )
 
 func Sync(reset bool) error {
-	var data map[string]interface{}
+	paginationToken := ""
 
-	syncToken := ""
-	var err error
+	for {
+		var data map[string]interface{}
 
-	if !reset {
-		syncToken, err = database.GetState("sync_token")
+		syncToken := ""
+		var err error
+
+		if !reset {
+			syncToken, err = database.GetState("sync_token")
+		}
+
+		if paginationToken != "" {
+			path := fmt.Sprintf("stacks/sync?pagination_token=%s", paginationToken)
+			data, err = Request(path, "GET")
+		} else if err != nil || reset {
+			path := fmt.Sprintf("stacks/sync?init=true")
+			data, err = Request(path, "GET")
+		} else {
+			path := fmt.Sprintf("stacks/sync?sync_token=%s", syncToken)
+			data, err = Request(path, "GET")
+		}
+
+		if err != nil {
+			return err
+		}
+
+		newSyncToken, hasNewSyncToken := data["sync_token"].(string)
+
+		if hasNewSyncToken {
+			database.SetState("sync_token", newSyncToken)
+		}
+
+		processSyncData(data)
+
+		var hasPaginationToken bool
+
+		paginationToken, hasPaginationToken = data["pagination_token"].(string)
+
+		if !hasPaginationToken {
+			break
+		}
 	}
-
-	if err != nil || reset {
-		path := fmt.Sprintf("stacks/sync?init=true")
-		data, err = Request(path, "GET")
-	} else {
-		path := fmt.Sprintf("stacks/sync?sync_token=%s", syncToken)
-		data, err = Request(path, "GET")
-	}
-
-	if err != nil {
-		return err
-	}
-
-	newSyncToken, hasNewSyncToken := data["sync_token"].(string)
-
-	if hasNewSyncToken {
-		database.SetState("sync_token", newSyncToken)
-	}
-
-	fmt.Println(data)
 
 	return nil
+}
+
+func processSyncData(data map[string]interface{}) {
+
 }
