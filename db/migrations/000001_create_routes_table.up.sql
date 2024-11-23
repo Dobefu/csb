589252b8f9b0466932f99733770ea167e3b02066
Connor van Spronssen <info@dobefu.com>
2024-11-23 11:57:10 +0100

    Add a parent field to the database

--- db/migrations/000001_create_routes_table.up.sql
+++ db/migrations/000001_create_routes_table.up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS routes(
   uuid varchar(64) NOT NULL PRIMARY KEY,
   slug varchar(255) NOT NULL,
-  url varchar(255) NOT NULL
+  url varchar(255) NOT NULL,
+  parent varchar(64)
 );
