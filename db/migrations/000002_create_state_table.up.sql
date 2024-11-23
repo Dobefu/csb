3c7f3c3f2f20c6d0244d1fb0a2263c153c72b362
Connor van Spronssen <info@dobefu.com>
2024-11-23 12:45:37 +0100

    Make the state name the key

--- db/migrations/000002_create_state_table.up.sql
+++ db/migrations/000002_create_state_table.up.sql
@@ -1,5 +1,4 @@
 CREATE TABLE IF NOT EXISTS state(
-  id int NOT NULL PRIMARY KEY UNIQUE,
-  name varchar(255) NOT NULL,
+  name varchar(255) NOT NULL PRIMARY KEY UNIQUE,
   value varchar(255) NOT NULL
 );
