b50b20371771ae42ea47ab8f3c8e7c7f6dded958
Connor van Spronssen <info@dobefu.com>
2024-11-24 14:31:52 +0100

    Add constants for log levels

--- cmd/logger/main.go
+++ cmd/logger/main.go
@@ -8,24 +8,38 @@ import (
 	"github.com/fatih/color"
 )
 
+var (
+	LOG_VERBOSE uint8 = 0
+	LOG_INFO    uint8 = 1
+	LOG_WARNING uint8 = 2
+	LOG_ERROR   uint8 = 3
+	LOG_FATAL   uint8 = 4
+)
+
+var LOGLEVEL uint8 = LOG_INFO
+
 func log(level byte, format string, a ...any) {
+	if level < LOGLEVEL {
+		return
+	}
+
 	timestamp := time.Now().Format(time.DateTime)
 	var str string
 
 	switch level {
-	case 0:
+	case LOG_VERBOSE:
 		str = fmt.Sprintf(color.HiBlackString("  %s"), format)
 		break
-	case 1:
+	case LOG_INFO:
 		str = fmt.Sprintf(color.HiCyanString("ℹ %s"), format)
 		break
-	case 2:
+	case LOG_WARNING:
 		str = fmt.Sprintf(color.YellowString("⚠ %s"), format)
 		break
-	case 3:
+	case LOG_ERROR:
 		str = fmt.Sprintf(color.HiRedString("‼ %s"), format)
 		break
-	case 4:
+	case LOG_FATAL:
 		str = fmt.Sprintf(color.RedString("‼ %s"), format)
 		break
 	default:
@@ -39,22 +53,26 @@ func log(level byte, format string, a ...any) {
 }
 
 func Verbose(format string, a ...any) {
-	log(0, format, a...)
+	log(LOG_VERBOSE, format, a...)
 }
 
 func Info(format string, a ...any) {
-	log(1, format, a...)
+	log(LOG_INFO, format, a...)
 }
 
 func Warning(format string, a ...any) {
-	log(2, format, a...)
+	log(LOG_WARNING, format, a...)
 }
 
 func Error(format string, a ...any) {
-	log(3, format, a...)
+	log(LOG_ERROR, format, a...)
 }
 
 func Fatal(format string, a ...any) {
-	log(4, format, a...)
+	log(LOG_FATAL, format, a...)
 	os.Exit(1)
+}
+
+func SetLogLevel(level uint8) {
+	LOGLEVEL = level
 }
