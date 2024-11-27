ffbc0d19cc3e43e6a180aab0b78f12962ae9d81f
Connor van Spronssen <info@dobefu.com>
2024-11-27 17:20:40 +0100

    Add support for SQLite3 databases, migration support will follow

--- cmd/database/main.go
+++ cmd/database/main.go
@@ -2,25 +2,29 @@ package database
 
 import (
 	"database/sql"
-	"fmt"
+	"errors"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var DB *sql.DB
 
 func Connect() error {
-	connString := fmt.Sprintf(
-		"%s:%s@tcp(%s:%s)/%s",
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASS"),
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_NAME"),
-	)
+	connString := os.Getenv("DB_CONN")
 
-	db, err := sql.Open("mysql", connString)
+	if connString == "" {
+		return errors.New("DB_CONN is not set")
+	}
+
+	dbType := os.Getenv("DB_TYPE")
+
+	if dbType == "" {
+		return errors.New("DB_TYPR is not set")
+	}
+
+	db, err := sql.Open(dbType, connString)
 
 	if err != nil {
 		return err
--- go.mod
+++ go.mod
@@ -17,6 +17,7 @@ require (
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-sqlite3 v1.14.24 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	golang.org/x/sys v0.27.0 // indirect
