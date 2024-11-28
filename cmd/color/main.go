63432e034b1803c3c8f95ec5f4d5f68ced824a79
Connor van Spronssen <info@dobefu.com>
2024-11-28 20:28:02 +0100

    Add a custom color package

---
+++ cmd/color/main.go
@@ -0,0 +1,30 @@
+package color
+
+const (
+	Reset int = iota
+	Bold
+	Dim
+	Italic
+	Underline
+)
+
+const (
+	FgBlack int = iota + 30
+	FgRed
+	FgGreen
+	FgYellow
+	FgBlue
+	FgMagenta
+	FgCyan
+)
+
+const (
+	FgDarkGray int = iota + 90
+	FgLightRed
+	FgLightGreen
+	FgLightYellow
+	FgLightBlue
+	FgLightMagenta
+	FgLightCyan
+	FgWhite
+)
