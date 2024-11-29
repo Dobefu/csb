d01d1120110987812403e7a1cd89768f0a07405f
Connor van Spronssen <info@dobefu.com>
2024-11-29 12:55:27 +0100

    Add a struct to construct a query

---
+++ cmd/database/structs/query.go
@@ -0,0 +1,15 @@
+package structs
+
+type QueryType int
+
+const (
+	QueryTypeSelect QueryType = iota
+	QueryTypeInsert
+	QueryTypeUpsert
+	QueryTypeUpdate
+	QueryTypeDelete
+)
+
+type Query struct {
+	Type QueryType
+}
