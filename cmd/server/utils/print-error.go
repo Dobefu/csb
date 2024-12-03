bed4d4d6a2841b835c7dc39cfb7bcdd70a8da629
Connor van Spronssen <info@dobefu.com>
2024-12-03 07:56:27 +0100

    Add string escape characters to the error JSON output

--- cmd/server/utils/print-error.go
+++ cmd/server/utils/print-error.go
@@ -3,8 +3,9 @@ package utils
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func PrintError(w http.ResponseWriter, err error) {
-	fmt.Fprintf(w, `{"data": null, "error": "%s"}`, err.Error())
+	fmt.Fprintf(w, `{"data": null, "error": "%s"}`, strings.ReplaceAll(err.Error(), `"`, `\"`))
 }
