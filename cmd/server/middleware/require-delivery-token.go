8e448cf19fa52e1e3f140115f251026dc55feaae
Connor van Spronssen <info@dobefu.com>
2024-12-06 15:38:31 +0100

    Add an environment variable to disable the auth token checks

--- cmd/server/middleware/require-delivery-token.go
+++ cmd/server/middleware/require-delivery-token.go
@@ -9,8 +9,24 @@ import (
 	"github.com/Dobefu/csb/cmd/server/utils"
 )
 
+func init() {
+	authDebug := os.Getenv("DEBUG_AUTH_BYPASS")
+
+	if authDebug != "" {
+		logger.Warning("DEBUG_AUTH_BYPASS is set. Running without any auth token checks")
+	}
+}
+
 func RequireDeliveryToken(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authDebug := os.Getenv("DEBUG_AUTH_BYPASS")
+
+		// If auth debugging is enabled, skip this middleware.
+		if authDebug != "" {
+			next.ServeHTTP(w, r)
+			return
+		}
+
 		token := os.Getenv("CS_DELIVERY_TOKEN")
 
 		if token == "" {
