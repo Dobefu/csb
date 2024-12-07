560d582f0ff103f909f65a9df807ed3a246374bb
Connor van Spronssen <info@dobefu.com>
2024-12-07 20:51:37 +0100

    Reduce code duplication in the ParseOperator test

--- cmd/database/utils/parse-operator_test.go
+++ cmd/database/utils/parse-operator_test.go
@@ -12,32 +12,18 @@ func TestParseOperator(t *testing.T) {
 	var operator string
 	var err error
 
-	os.Setenv("DB_TYPE", "mysql")
+	dbTypes := []string{"mysql", "sqlite3", "postgres"}
 
-	operator, err = ParseOperator(structs.EQUALS)
-	assert.Equal(t, operator, "=")
-	assert.Equal(t, err, nil)
-	operator, err = ParseOperator(structs.NOT_EQUALS)
-	assert.Equal(t, operator, "<>")
-	assert.Equal(t, err, nil)
+	for _, dbType := range dbTypes {
+		os.Setenv("DB_TYPE", dbType)
 
-	os.Setenv("DB_TYPE", "sqlite3")
-
-	operator, err = ParseOperator(structs.EQUALS)
-	assert.Equal(t, operator, "=")
-	assert.Equal(t, err, nil)
-	operator, err = ParseOperator(structs.NOT_EQUALS)
-	assert.Equal(t, operator, "<>")
-	assert.Equal(t, err, nil)
-
-	os.Setenv("DB_TYPE", "postgres")
-
-	operator, err = ParseOperator(structs.EQUALS)
-	assert.Equal(t, operator, "=")
-	assert.Equal(t, err, nil)
-	operator, err = ParseOperator(structs.NOT_EQUALS)
-	assert.Equal(t, operator, "<>")
-	assert.Equal(t, err, nil)
+		operator, err = ParseOperator(structs.EQUALS)
+		assert.Equal(t, operator, "=")
+		assert.Equal(t, err, nil)
+		operator, err = ParseOperator(structs.NOT_EQUALS)
+		assert.Equal(t, operator, "<>")
+		assert.Equal(t, err, nil)
+	}
 
 	operator, err = ParseOperator(9999)
 	assert.Equal(t, operator, "")
