d2d53080835b3bdf877eef6179103ab97344a3f3
Connor van Spronssen <info@dobefu.com>
2024-12-22 15:31:39 +0100

    feat(sdk): Add a function to create a global field

---
+++ cmd/cs_sdk/api/create-global-field.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	"errors"
+
+	"github.com/Dobefu/csb/cmd/cs_sdk"
+	"github.com/Dobefu/csb/cmd/logger"
+)
+
+func CreateGlobalField(name string, id string, data map[string]interface{}) error {
+	globalField := GetGlobalField(id)
+
+	if globalField != nil {
+		return errors.New("The global field already exists")
+	}
+
+	_, err := cs_sdk.Request(
+		"global_fields",
+		"POST",
+		data,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	logger.Info("The global field has been created")
+
+	return nil
+}
