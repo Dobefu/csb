2e7945d70f3c33e625dbe5783d22dadc7e12320c
Connor van Spronssen <info@dobefu.com>
2024-12-22 15:25:03 +0100

    feat(sdk): Add a function to get a global field

---
+++ cmd/cs_sdk/api/get-global-field.go
@@ -0,0 +1,21 @@
+package api
+
+import (
+	"fmt"
+
+	"github.com/Dobefu/csb/cmd/cs_sdk"
+)
+
+func GetGlobalField(id string) interface{} {
+	globalField, err := cs_sdk.Request(
+		fmt.Sprintf("global_fields/%s", id),
+		"GET",
+		nil,
+	)
+
+	if err != nil {
+		return nil
+	}
+
+	return globalField
+}
