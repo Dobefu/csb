a25b4d48ee0090640f139f80fd71d2224f3e60b8
Connor van Spronssen <info@dobefu.com>
2024-12-25 20:18:21 +0100

    feat: add an ancestor field to the Route struct

--- cmd/cs_sdk/structs/route.go
+++ cmd/cs_sdk/structs/route.go
@@ -8,6 +8,7 @@ type Route struct {
 	Slug           string `json:"slug"`
 	Url            string `json:"url"`
 	Parent         string `json:"parent"`
+	Ancestor       string `json:"ancestor"`
 	ExcludeSitemap bool   `json:"exclude_sitemap"`
 	Published      bool   `json:"published"`
 }
