cd17699fcd24823e12ce5e6577a58e0cf0dfebb9
Connor van Spronssen <info@dobefu.com>
2024-12-26 13:55:36 +0100

    feat: Add common security headers to all API responses

--- cmd/server/middleware/add-response-headers.go
+++ cmd/server/middleware/add-response-headers.go
@@ -18,6 +18,8 @@ func init() {
 func AddResponseHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "application/json")
+		w.Header().Add("X-Content-Type-Options", "nosniff")
+		w.Header().Add("X-Frame-Options", "DENY")
 
 		next.ServeHTTP(w, r)
 	})
