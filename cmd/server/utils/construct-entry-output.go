c9ecf3a77c3b5a989fcacdcb74e1c4e566f5a676
Connor van Spronssen <info@dobefu.com>
2024-12-26 06:27:42 +0100

    feat: Make the ConstructEntryOutput function require the breadcrumbs

--- cmd/server/utils/construct-entry-output.go
+++ cmd/server/utils/construct-entry-output.go
@@ -1,14 +1,20 @@
 package utils
 
 import (
-	"github.com/Dobefu/csb/cmd/api/structs"
+	api_structs "github.com/Dobefu/csb/cmd/api/structs"
+	"github.com/Dobefu/csb/cmd/cs_sdk/structs"
 )
 
-func ConstructEntryOutput(entry interface{}, altLocales []structs.AltLocale) map[string]map[string]interface{} {
+func ConstructEntryOutput(
+	entry interface{},
+	altLocales []api_structs.AltLocale,
+	breadcrumbs []structs.Route,
+) map[string]map[string]interface{} {
 	output := ConstructOutput()
 
 	output["data"]["entry"] = entry
 	output["data"]["alt_locales"] = altLocales
+	output["data"]["breadcrumbs"] = breadcrumbs
 
 	return output
 }
