4655881806957569e8cb73a722b7d33d00d264bf
Connor van Spronssen <info@dobefu.com>
2024-12-28 19:47:07 +0100

    feat: Add a UID column to the translations table

--- cmd/migrate_db/migrations/000003_create_translations_table.up.sql
+++ cmd/migrate_db/migrations/000003_create_translations_table.up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS translations(
   source varchar(128) NOT NULL PRIMARY KEY UNIQUE,
   translation varchar(128) NOT NULL,
-  category varchar(255) NOT NULL
+  category varchar(255) NOT NULL,
+  uid varchar(32) NOT NULL
 );
