d43f454a3015a4005050f7be697bf4402b218ba5
Connor van Spronssen <info@dobefu.com>
2024-12-28 21:36:11 +0100

    feat: Add a locale column to the translations table

--- cmd/migrate_db/migrations/000003_create_translations_table.up.sql
+++ cmd/migrate_db/migrations/000003_create_translations_table.up.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS translations(
-  source varchar(128) NOT NULL PRIMARY KEY UNIQUE,
+  id varchar(48) NOT NULL PRIMARY KEY UNIQUE,
+  uid varchar(32) NOT NULL,
+  source varchar(128) NOT NULL,
   translation varchar(128) NOT NULL,
   category varchar(255) NOT NULL,
-  uid varchar(32) NOT NULL
+  locale varchar(16) NOT NULL
 );
