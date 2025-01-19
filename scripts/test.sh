a747e43f5f77b7dfd7ce5876bce9b10504dd6729
Connor van Spronssen <info@dobefu.com>
2025-01-19 14:25:42 +0100

    test: Update the test script to exclude test_utils.go

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,13 +3,16 @@
 # Exit on error.
 set -e
 
-# Get the directory where the current script is located.
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-
 # Navigate to the root of the project.
-cd "$SCRIPT_DIR/.."
+cd "$(dirname "$0")/.."
 
 # Run the tests.
 go test "./..." -coverprofile="coverage.out" -covermode=count
+
+# Filter out test_utils.go from the coverage report.
+grep -v "test_utils.go" coverage.out > coverage.tmp
+mv coverage.tmp coverage.out
+
+# Display the coverage statistics and generate an HTML report.
 go tool cover -func coverage.out
 go tool cover -html=coverage.out
