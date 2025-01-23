92f8ac05d059d87f7e5d59544dd479af508de64a
Connor van Spronssen <info@dobefu.com>
2025-01-23 20:52:37 +0100

    test: Redo tests for the HandleRoutes function

---
+++ cmd/server/handle-routes_test.go
@@ -0,0 +1,23 @@
+package server
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHandleRoutes(t *testing.T) {
+	mux := http.NewServeMux()
+	apiPath := "/api"
+	HandleRoutes(mux, apiPath)
+
+	req, err := http.NewRequest("GET", "/", nil)
+	assert.NoError(t, err)
+
+	rr := httptest.NewRecorder()
+	mux.ServeHTTP(rr, req)
+	assert.Equal(t, http.StatusOK, rr.Code)
+
+}
