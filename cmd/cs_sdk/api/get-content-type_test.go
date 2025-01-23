2edace7e9474b97ce0d95f4b9c1fda3802e5cf40
Connor van Spronssen <info@dobefu.com>
2025-01-23 07:49:25 +0100

    test: Redo tests for the GetContentType function

--- cmd/cs_sdk/api/get-content-type_test.go
+++ cmd/cs_sdk/api/get-content-type_test.go
@@ -1,20 +1,39 @@
 package api
 
 import (
+	"errors"
 	"testing"
 
-	"github.com/Dobefu/csb/cmd/init_env"
+	"github.com/Dobefu/csb/cmd/cs_sdk"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetContentType(t *testing.T) {
-	init_env.Main("../../../.env.test")
+func setupGetContentTypeTest() func() {
+	return func() {
+		csSdkRequest = cs_sdk.Request
+	}
+}
 
-	var contentType interface{}
+func TestGetContentTypeSuccess(t *testing.T) {
+	cleanup := setupGetContentTypeTest()
+	defer cleanup()
 
-	contentType = GetContentType("basic_page")
-	assert.NotEqual(t, nil, contentType)
+	csSdkRequest = func(path string, method string, body map[string]interface{}, useManagementToken bool) (map[string]interface{}, error) {
+		return map[string]interface{}{}, nil
+	}
 
-	contentType = GetContentType("bogus")
+	contentType := GetContentType("Test Name")
+	assert.Equal(t, map[string]interface{}{}, contentType)
+}
+
+func TestGetContentTypeErrNotFound(t *testing.T) {
+	cleanup := setupGetContentTypeTest()
+	defer cleanup()
+
+	csSdkRequest = func(path string, method string, body map[string]interface{}, useManagementToken bool) (map[string]interface{}, error) {
+		return nil, errors.New("cannot find content type")
+	}
+
+	contentType := GetContentType("Bogus")
 	assert.Equal(t, nil, contentType)
 }
