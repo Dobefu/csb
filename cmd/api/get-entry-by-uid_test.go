3705ccd5a0df8bcacfdc8f7149768d38fc0dce9a
Connor van Spronssen <info@dobefu.com>
2025-01-24 09:55:38 +0100

    test: Redo tests for the GetEntryByUid function

--- cmd/api/get-entry-by-uid_test.go
+++ cmd/api/get-entry-by-uid_test.go
@@ -1,38 +1,53 @@
 package api
 
 import (
-	"os"
+	"database/sql"
 	"testing"
+	"time"
 
-	"github.com/Dobefu/csb/cmd/database"
-	"github.com/Dobefu/csb/cmd/init_env"
-	"github.com/Dobefu/csb/cmd/migrate_db"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/Dobefu/csb/cmd/cs_sdk/structs"
+	"github.com/Dobefu/csb/cmd/database/query"
+	db_structs "github.com/Dobefu/csb/cmd/database/structs"
 	"github.com/stretchr/testify/assert"
 )
 
+func setupGetEntryByUidTest(t *testing.T) (sqlmock.Sqlmock, func()) {
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+
+	queryQueryRow = func(table string, fields []string, where []db_structs.QueryWhere) *sql.Row {
+		return db.QueryRow("SELECT (.+) FROM routes", nil)
+	}
+
+	return mock, func() {
+		queryQueryRow = query.QueryRow
+	}
+}
+
 func TestGetEntryByUid(t *testing.T) {
-	init_env.Main("../../.env.test")
-	err := database.Connect()
-	assert.Equal(t, nil, err)
+	mock, cleanup := setupGetEntryByUidTest(t)
+	defer cleanup()
 
-	err = migrate_db.Main(true)
-	assert.Equal(t, nil, err)
+	cols := []string{"id", "uid", "title", "content_type", "locale", "slug", "url", "parent", "version", "updated_at", "exclude_sitemap", "published"}
+	mock.ExpectQuery("SELECT (.+) FROM routes").WillReturnRows(
+		sqlmock.NewRows(cols).AddRow("id", "uid", "title", "content_type", "locale", "slug", "url", "parent", 0, time.Time{}, true, true),
+	)
 
-	oldDb := os.Getenv("DB_CONN")
-	os.Setenv("DB_CONN", "file:/")
-	err = database.Connect()
-	assert.Equal(t, nil, err)
-
-	_, err = GetEntryByUid("", "", false)
-	assert.NotEqual(t, nil, err)
-
-	os.Setenv("DB_CONN", oldDb)
-	err = database.Connect()
-	assert.Equal(t, nil, err)
-
-	err = insertPage("testingen", "testing", "")
-	assert.Equal(t, nil, err)
-
-	_, err = GetEntryByUid("testing", "en", true)
-	assert.Equal(t, nil, err)
+	entry, err := GetEntryByUid("test-uid", "test-locale", false)
+	assert.NoError(t, err)
+	assert.Equal(t, structs.Route{
+		Id:             "id",
+		Uid:            "uid",
+		Title:          "title",
+		ContentType:    "content_type",
+		Locale:         "locale",
+		Slug:           "slug",
+		Url:            "url",
+		Parent:         "parent",
+		Version:        0,
+		UpdatedAt:      time.Time{},
+		ExcludeSitemap: true,
+		Published:      true,
+	}, entry)
 }
