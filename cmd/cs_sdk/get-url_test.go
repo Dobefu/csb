e8d363a297eef4ee4dc00e5f4d796c7a9d13b4eb
Connor van Spronssen <info@dobefu.com>
2025-01-24 08:54:02 +0100

    test: Redo tests for the GetUrl function

--- cmd/cs_sdk/get-url_test.go
+++ cmd/cs_sdk/get-url_test.go
@@ -4,25 +4,43 @@ import (
 	"os"
 	"testing"
 
-	"github.com/Dobefu/csb/cmd/init_env"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetUrl(t *testing.T) {
-	init_env.Main("../../.env.test")
-	oldCsRegion := os.Getenv("CS_REGION")
+func setupGetUrlTest() func() {
+	return func() {
+		os.Unsetenv("CS_REGION")
+	}
+}
+
+func TestGetUrlEu(t *testing.T) {
+	cleanup := setupGetUrlTest()
+	defer cleanup()
 
 	os.Setenv("CS_REGION", "eu")
 	assert.Equal(t, GetUrl(true), "https://eu-api.contentstack.com")
+}
+
+func TestGetUrlUs(t *testing.T) {
+	cleanup := setupGetUrlTest()
+	defer cleanup()
 
 	os.Setenv("CS_REGION", "us")
 	assert.Equal(t, GetUrl(true), "https://api.contentstack.io")
+}
+
+func TestGetUrlAzureNa(t *testing.T) {
+	cleanup := setupGetUrlTest()
+	defer cleanup()
 
 	os.Setenv("CS_REGION", "azure-na")
 	assert.Equal(t, GetUrl(true), "https://azure-na-api.contentstack.com")
+}
+
+func TestGetUrlAzureEu(t *testing.T) {
+	cleanup := setupGetUrlTest()
+	defer cleanup()
 
 	os.Setenv("CS_REGION", "azure-eu")
 	assert.Equal(t, GetUrl(true), "https://azure-eu-api.contentstack.com")
-
-	os.Setenv("CS_REGION", oldCsRegion)
 }
